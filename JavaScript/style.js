46cdfb8b8e77b6decf6d59c193431e154e0601b1
kaliyappan <141757796+Kaliyappan1@users.noreply.github.com>
2024-01-06 12:52:47 +0530

    Add files via upload

---
+++ JavaScript/style.js
@@ -0,0 +1,514 @@
+let task_create = [
+    
+        {
+            "userId": kaliyappan,
+            "id": 1,
+            "title": "delectus aut autem",
+            "completed": false
+        },
+        {
+            "userId": kaliyappan,
+            "id": 2,
+            "title": "quis ut nam facilis et officia qui",
+            "completed": false
+        },
+        {
+            "userId": kaliyappan,
+            "id": 3,
+            "title": "fugiat veniam minus",
+            "completed": false
+        },
+        {
+            "userId": kaliyappan,
+            "id": 4,
+            "title": "et porro tempora",
+            "completed": true
+        },
+        {
+            "userId": kaliyappan,
+            "id": 5,
+            "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+            "completed": false
+        },
+        {
+            "userId": kaliyappan,
+            "id": 6,
+            "title": "qui ullam ratione quibusdam voluptatem quia omnis",
+            "completed": false
+        },
+        {
+            "userId": kaliyappan,
+            "id": 7,
+            "title": "illo expedita consequatur quia in",
+            "completed": false
+        },
+        {
+            "userId": kaliyappan,
+            "id": 8,
+            "title": "quo adipisci enim quam ut ab",
+            "completed": true
+        },
+        
+        {
+            "userId": 2,
+            "id": 21,
+            "title": "suscipit repellat esse quibusdam voluptatem incidunt",
+            "completed": false
+        },
+        {
+            "userId": 2,
+            "id": 22,
+            "title": "distinctio vitae autem nihil ut molestias quo",
+            "completed": true
+        },
+        {
+            "userId": 2,
+            "id": 23,
+            "title": "et itaque necessitatibus maxime molestiae qui quas velit",
+            "completed": false
+        },
+        {
+            "userId": 2,
+            "id": 24,
+            "title": "adipisci non ad dicta qui amet quaerat doloribus ea",
+            "completed": false
+        },
+        {
+            "userId": 2,
+            "id": 25,
+            "title": "voluptas quo tenetur perspiciatis explicabo natus",
+            "completed": true
+        },
+        {
+            "userId": 2,
+            "id": 26,
+            "title": "aliquam aut quasi",
+            "completed": true
+        },
+        {
+            "userId": 2,
+            "id": 27,
+            "title": "veritatis pariatur delectus",
+            "completed": true
+        },
+        {
+            "userId": 2,
+            "id": 28,
+            "title": "nesciunt totam sit blanditiis sit",
+            "completed": false
+        },
+        
+        {
+            "userId": 3,
+            "id": 41,
+            "title": "aliquid amet impedit consequatur aspernatur placeat eaque fugiat suscipit",
+            "completed": false
+        },
+        {
+            "userId": 3,
+            "id": 42,
+            "title": "rerum perferendis error quia ut eveniet",
+            "completed": false
+        },
+        {
+            "userId": 3,
+            "id": 43,
+            "title": "tempore ut sint quis recusandae",
+            "completed": true
+        },
+        {
+            "userId": 3,
+            "id": 44,
+            "title": "cum debitis quis accusamus doloremque ipsa natus sapiente omnis",
+            "completed": true
+        },
+        {
+            "userId": 3,
+            "id": 45,
+            "title": "velit soluta adipisci molestias reiciendis harum",
+            "completed": false
+        },
+        {
+            "userId": 3,
+            "id": 46,
+            "title": "vel voluptatem repellat nihil placeat corporis",
+            "completed": false
+        },
+        {
+            "userId": 3,
+            "id": 47,
+            "title": "nam qui rerum fugiat accusamus",
+            "completed": false
+        },
+        {
+            "userId": 3,
+            "id": 48,
+            "title": "sit reprehenderit omnis quia",
+            "completed": false
+        },
+        
+        {
+            "userId": 4,
+            "id": 61,
+            "title": "odit optio omnis qui sunt",
+            "completed": true
+        },
+        {
+            "userId": 4,
+            "id": 62,
+            "title": "et placeat et tempore aspernatur sint numquam",
+            "completed": false
+        },
+        {
+            "userId": 4,
+            "id": 63,
+            "title": "doloremque aut dolores quidem fuga qui nulla",
+            "completed": true
+        },
+        {
+            "userId": 4,
+            "id": 64,
+            "title": "voluptas consequatur qui ut quia magnam nemo esse",
+            "completed": false
+        },
+        {
+            "userId": 4,
+            "id": 65,
+            "title": "fugiat pariatur ratione ut asperiores necessitatibus magni",
+            "completed": false
+        },
+        {
+            "userId": 4,
+            "id": 66,
+            "title": "rerum eum molestias autem voluptatum sit optio",
+            "completed": false
+        },
+        {
+            "userId": 4,
+            "id": 67,
+            "title": "quia voluptatibus voluptatem quos similique maiores repellat",
+            "completed": false
+        },
+        {
+            "userId": 4,
+            "id": 68,
+            "title": "aut id perspiciatis voluptatem iusto",
+            "completed": false
+        },
+       
+        {
+            "userId": 5,
+            "id": 81,
+            "title": "suscipit qui totam",
+            "completed": true
+        },
+        {
+            "userId": 5,
+            "id": 82,
+            "title": "voluptates eum voluptas et dicta",
+            "completed": false
+        },
+        {
+            "userId": 5,
+            "id": 83,
+            "title": "quidem at rerum quis ex aut sit quam",
+            "completed": true
+        },
+        {
+            "userId": 5,
+            "id": 84,
+            "title": "sunt veritatis ut voluptate",
+            "completed": false
+        },
+        {
+            "userId": 5,
+            "id": 85,
+            "title": "et quia ad iste a",
+            "completed": true
+        },
+        {
+            "userId": 5,
+            "id": 86,
+            "title": "incidunt ut saepe autem",
+            "completed": true
+        },
+        {
+            "userId": 5,
+            "id": 87,
+            "title": "laudantium quae eligendi consequatur quia et vero autem",
+            "completed": true
+        },
+        {
+            "userId": 5,
+            "id": 88,
+            "title": "vitae aut excepturi laboriosam sint aliquam et et accusantium",
+            "completed": false
+        },
+        {
+            "userId": 6,
+            "id": 101,
+            "title": "explicabo enim cumque porro aperiam occaecati minima",
+            "completed": false
+        },
+        {
+            "userId": 6,
+            "id": 102,
+            "title": "sed ab consequatur",
+            "completed": false
+        },
+        {
+            "userId": 6,
+            "id": 103,
+            "title": "non sunt delectus illo nulla tenetur enim omnis",
+            "completed": false
+        },
+        {
+            "userId": 6,
+            "id": 104,
+            "title": "excepturi non laudantium quo",
+            "completed": false
+        },
+        {
+            "userId": 6,
+            "id": 105,
+            "title": "totam quia dolorem et illum repellat voluptas optio",
+            "completed": true
+        },
+        {
+            "userId": 6,
+            "id": 106,
+            "title": "ad illo quis voluptatem temporibus",
+            "completed": true
+        },
+        {
+            "userId": 6,
+            "id": 107,
+            "title": "praesentium facilis omnis laudantium fugit ad iusto nihil nesciunt",
+            "completed": false
+        },
+        {
+            "userId": 6,
+            "id": 108,
+            "title": "a eos eaque nihil et exercitationem incidunt delectus",
+            "completed": true
+        },
+        
+        {
+            "userId": 7,
+            "id": 121,
+            "title": "inventore aut nihil minima laudantium hic qui omnis",
+            "completed": true
+        },
+        {
+            "userId": 7,
+            "id": 122,
+            "title": "provident aut nobis culpa",
+            "completed": true
+        },
+        {
+            "userId": 7,
+            "id": 123,
+            "title": "esse et quis iste est earum aut impedit",
+            "completed": false
+        },
+        {
+            "userId": 7,
+            "id": 124,
+            "title": "qui consectetur id",
+            "completed": false
+        },
+        {
+            "userId": 7,
+            "id": 125,
+            "title": "aut quasi autem iste tempore illum possimus",
+            "completed": false
+        },
+        {
+            "userId": 7,
+            "id": 126,
+            "title": "ut asperiores perspiciatis veniam ipsum rerum saepe",
+            "completed": true
+        },
+        {
+            "userId": 7,
+            "id": 127,
+            "title": "voluptatem libero consectetur rerum ut",
+            "completed": true
+        },
+        {
+            "userId": 7,
+            "id": 128,
+            "title": "eius omnis est qui voluptatem autem",
+            "completed": false
+        },
+       
+        {
+            "userId": 8,
+            "id": 141,
+            "title": "explicabo consectetur debitis voluptates quas quae culpa rerum non",
+            "completed": true
+        },
+        {
+            "userId": 8,
+            "id": 142,
+            "title": "maiores accusantium architecto necessitatibus reiciendis ea aut",
+            "completed": true
+        },
+        {
+            "userId": 8,
+            "id": 143,
+            "title": "eum non recusandae cupiditate animi",
+            "completed": false
+        },
+        {
+            "userId": 8,
+            "id": 144,
+            "title": "ut eum exercitationem sint",
+            "completed": false
+        },
+        {
+            "userId": 8,
+            "id": 145,
+            "title": "beatae qui ullam incidunt voluptatem non nisi aliquam",
+            "completed": false
+        },
+        {
+            "userId": 8,
+            "id": 146,
+            "title": "molestiae suscipit ratione nihil odio libero impedit vero totam",
+            "completed": true
+        },
+        {
+            "userId": 8,
+            "id": 147,
+            "title": "eum itaque quod reprehenderit et facilis dolor autem ut",
+            "completed": true
+        },
+        {
+            "userId": 8,
+            "id": 148,
+            "title": "esse quas et quo quasi exercitationem",
+            "completed": false
+        },
+       
+        {
+            "userId": 9,
+            "id": 161,
+            "title": "ex hic consequuntur earum omnis alias ut occaecati culpa",
+            "completed": true
+        },
+        {
+            "userId": 9,
+            "id": 162,
+            "title": "omnis laboriosam molestias animi sunt dolore",
+            "completed": true
+        },
+        {
+            "userId": 9,
+            "id": 163,
+            "title": "natus corrupti maxime laudantium et voluptatem laboriosam odit",
+            "completed": false
+        },
+        {
+            "userId": 9,
+            "id": 164,
+            "title": "reprehenderit quos aut aut consequatur est sed",
+            "completed": false
+        },
+        {
+            "userId": 9,
+            "id": 165,
+            "title": "fugiat perferendis sed aut quidem",
+            "completed": false
+        },
+        {
+            "userId": 9,
+            "id": 166,
+            "title": "quos quo possimus suscipit minima ut",
+            "completed": false
+        },
+        {
+            "userId": 9,
+            "id": 167,
+            "title": "et quis minus quo a asperiores molestiae",
+            "completed": false
+        },
+        {
+            "userId": 9,
+            "id": 168,
+            "title": "recusandae quia qui sunt libero",
+            "completed": false
+        },
+        
+        {
+            "userId": 10,
+            "id": 181,
+            "title": "ut cupiditate sequi aliquam fuga maiores",
+            "completed": false
+        },
+        {
+            "userId": 10,
+            "id": 182,
+            "title": "inventore saepe cumque et aut illum enim",
+            "completed": true
+        },
+        {
+            "userId": 10,
+            "id": 183,
+            "title": "omnis nulla eum aliquam distinctio",
+            "completed": true
+        },
+        {
+            "userId": 10,
+            "id": 184,
+            "title": "molestias modi perferendis perspiciatis",
+            "completed": false
+        },
+        {
+            "userId": 10,
+            "id": 185,
+            "title": "voluptates dignissimos sed doloribus animi quaerat aut",
+            "completed": false
+        },
+        {
+            "userId": 10,
+            "id": 186,
+            "title": "explicabo odio est et",
+            "completed": false
+        },
+        {
+            "userId": 10,
+            "id": 187,
+            "title": "consequuntur animi possimus",
+            "completed": false
+        },
+        {
+            "userId": 10,
+            "id": 188,
+            "title": "vel non beatae est",
+            "completed": true
+        },
+       
+]
+
+
+// show card data
+
+funtion showtable(task_viewer) {
+    
+    document.getElementById('searchInput').addEventListener('input', function() {
+        var searchValue = this.value.toLowerCase();
+        var cardText = document.querySelector('.card .card-body p.card-text');
+    
+        if (cardText) {
+          var cardTextContent = cardText.textContent.toLowerCase();
+          if (cardTextContent.includes(searchValue)) {
+            document.querySelector('.card').style.display = 'block';
+          } else {
+            document.querySelector('.card').style.display = 'none';
+          }
+        }
+      });
+    
+        
+}
